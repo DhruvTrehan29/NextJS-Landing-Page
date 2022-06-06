4d16cc0a35dcd33a2d32535ddfc97b2f4c26e506
DhruvTrehan29 <dhruvtrehan29@gmail.com>
2022-06-06 20:10:55 +0530

    Second Commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>This is the about page of this Next App</div>
+  )
+}
+
+export default about
--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Next.js landing page
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+        
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
