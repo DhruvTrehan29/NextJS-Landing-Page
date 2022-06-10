c548d81455ee0a2adba97d0e705f9e755b6dab00
DhruvTrehan29 <dhruvtrehan29@gmail.com>
2022-06-10 19:09:39 +0530

    Sixth Commit

---
+++ pages/api/Blogs.js
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from 'fs';
+export default async function handler(req, res) {
+  let data=await fs.promises.readdir("BlogData")
+  let MyFile;
+  let allBlogs=[];
+  for(let index=0;index<data.length;index++){
+    const item=data[index];
+     console.log(item)
+     MyFile= await fs.promises.readFile(("BlogData/"+item),'utf-8')
+     allBlogs.push(JSON.parse(MyFile))
+  }
+  res.status(200).json(allBlogs)
+}
--- pages/blog.js
+++ pages/blog.js
@@ -1,34 +1,31 @@
-import React from "react";
+
+import React, {useEffect,useState} from 'react'; 
 import Link from "next/Link";
 import styles from "../styles/Blog.module.css";
 const Blog = () => {
-  return (
-    <div className={styles.container}>
+  const [Blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    console.log("UseEffect is running")
+    fetch('http://localhost:3000/api/Blogs').then((a)=>{
+        return a.json();  })
+        .then((parsed)=>{
+          console.log(parsed)
+          setBlogs(parsed)
+        })
+      },[])
+  
+  return <div className={styles.container}>
           <main className={styles.main}>
-          <span className={styles.MyPersonalBlogHeading}>Personal Blog List</span>
-          <div>
-          <Link href={'/BlogPost/Java-learn'}>
-          <h3 className={styles.blogItem}>1. How to learn Java ?</h3></Link>
-          <p>This blog will teach you how to run programs using Java Programming Language.</p>
+          <h2 className={styles.MyPersonalBlogHeading}>Popular Blogs</h2>
+           {Blogs.map((blogitem)=>{
+                return <div key={blogitem.slug}>
+                <Link href={`/BlogPost/${blogitem.slug}`}>
+                <h3 className={styles.blogItem}>{blogitem.title}</h3></Link>
+                <p className={styles.parasize}>{blogitem.content.substr(0,140)} ....</p>
         </div>
-        <div>
-            <Link href={'/BlogPost/Python-learn'}>
-          <h3 className={styles.blogItem}>2. How to learn Python ? </h3></Link>
-          <p> This blog will teach you how to run programs using Java Programming Language. </p>
-        </div>
-        <div >
-             <Link href={'/BlogPost/Python-learn'}>
-          <h3 className={styles.blogItem}>3. How to learn C++ ? </h3></Link>
-          <p> This blog will teach you how to run programs using Java Programming Language. </p>
-        </div>
-        <div>
-             <Link href={'/BlogPost/Python-learn'}>
-          <h3 className={styles.blogItem}>4. How to learn JavaScript ? </h3></Link>
-          <p>This blog will teach you how to run programs using Java Programming Language. </p>
-        </div>
+            })}
       </main>
     </div>
-  );
 };
 
 export default Blog;
